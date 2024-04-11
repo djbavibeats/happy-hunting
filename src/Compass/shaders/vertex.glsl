81e0e8e63dfe15b47f673748b3c56a66d029642d
djbavibeats <justin@voltcreative.com>
2024-04-10 23:50:01 -0400

    Add to vertex shader

--- src/Compass/shaders/vertex.glsl
+++ src/Compass/shaders/vertex.glsl
@@ -1,8 +1,25 @@
+uniform float time;
+uniform float intensity;
+
 varying vec2 vUvs;
 
-void main() {	
-  vec4 localPosition = vec4(position, 1.0);
+float inverseLerp(float v, float minValue, float maxValue) {
+  return (v - minValue) / (maxValue - minValue);
+}
 
-  gl_Position = projectionMatrix * modelViewMatrix * localPosition;
+float remap(float v, float inMin, float inMax, float outMin, float outMax) {
+  float t = inverseLerp(v, inMin, inMax);
+  return mix(outMin, outMax, t);
+}
+
+void main() {	
+  float factor = remap(intensity, 0.0, 1.0, 0.0, 25.0);
+  vec3 localSpacePosition = position;
+
+  vec4 modelPosition = modelViewMatrix * vec4(localSpacePosition, 1.0);
+  modelPosition.x += sin(modelPosition.y + time * 5.0) * factor;
+  // modelPosition.x += sin(modelPosition.y + time * 2.5) * 25.0;
+
+  gl_Position = projectionMatrix * modelPosition;
   vUvs = uv;
 }
